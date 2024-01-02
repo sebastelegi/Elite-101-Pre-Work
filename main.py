209eda8e9591ef6fb350e9af5e112c5b73904c75
sebastelegi <154757901+sebastelegi@users.noreply.github.com>
2024-01-02 00:28:32 +0000

    coded template for chatbot
    coded the first parts of the chatbot

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-i = 1
-while i<=6:
-  print(i)
-  i += 2
+print('Welcome to Elite 101 Chatbot!')
+name = input('Please enter your name:')
+age = int(input(f'Nice to meet you {name}! How old are you? '))
+if age < 30:
+  print(f'Welcome {name}! Oh, to be {age} again! How can I help you today?')
+elif age > 30:
+  print(f'Welcome {name}! {age} is such a wise age! How can I help you today?')
+  
+x = True
+
+while x == True:
+  print('Please choose from the following options:')
+  print('1. Placeholder Option 1')
+  print('2. Placeholder Option 2')
+  print('3. Placeholder Option 3')
+  print('4. Exit the conversation')
+  choice = int(input('Enter the number of your choice: '))
+  if choice == 4:
+    print(f'Goodbye, {name}! Have a great day!')
+    x = False
